988b424fae9331fa833f02aa2f64f4ae0716700c
wangzhiwei <wangzhiwei@192.168.1.141>
2016-10-12 17:39:31 +0800

    demo modify

---
+++ modules/foxbpm-webapps-common/src/main/java/org/foxbpm/rest/JavaRESTClient.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package org.foxbpm.rest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * @author wangzhiwei
+ *
+ */
+public class JavaRESTClient {
+
+	/**
+	 * 
+	 */
+	private JavaRESTClient() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static String requestURL(String targetURL, String method,
+			String params) {
+		StringBuffer buffer = new StringBuffer();
+		
+		try {
+			URL restServiceURL = new URL(targetURL);
+
+			HttpURLConnection httpConnection = (HttpURLConnection) restServiceURL.openConnection();
+			httpConnection.setDoOutput(true);
+			httpConnection.setRequestMethod("POST");	//	POST|GET
+			httpConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+			// 入参
+			OutputStream outputStream = httpConnection.getOutputStream();
+            outputStream.write(params.getBytes());
+            outputStream.flush();
+			
+			if (httpConnection.getResponseCode() != 200) {
+				throw new RuntimeException(
+						"HTTP GET Request Failed with Error code : " + httpConnection.getResponseCode());
+			}
+
+            // 出参
+			BufferedReader responseBuffer = new BufferedReader(
+					new InputStreamReader((httpConnection.getInputStream())));
+			
+			String output = null;
+			System.out.println("Output from Server:");
+
+			while ((output = responseBuffer.readLine()) != null) {
+				System.out.println(output);
+				buffer.append(output);
+			}
+
+			httpConnection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return buffer.toString();
+	}
+
+	public static void main(String[] args) {
+		String params = "userName=admin&password=1"
+				+ "&expenseId=BXD-20161011055100008&createTime=2016-09-11&ownerName=超级管理员&owner=admin&dept=200011&account=2&invoiceType=1&reason=不喜欢你"
+				+ "&flowCommandInfo={\"processDefinitionKey\":\"tt_1\",\"processInstanceId\":\"\",\"taskId\":\"\","
+				+ "\"commandId\":\"HandleCommand_2\",\"commandType\":\"startandsubmit\",\"bizKey\":\"BXD-20161011055100008\",\"taskComment\":\"\"}";
+		requestURL("http://localhost:8080/foxbpm-webapps-common/expenses.action", 
+				"POST", params);
+	}
+}
--- modules/foxbpm-webapps-portal/src/main/java/org/foxbpm/portal/controller/ExpenseController.java
+++ modules/foxbpm-webapps-portal/src/main/java/org/foxbpm/portal/controller/ExpenseController.java
@@ -20,10 +20,13 @@ package org.foxbpm.portal.controller;
 import java.io.IOException;
 import java.util.Map;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.foxbpm.engine.impl.entity.UserEntity;
+import org.foxbpm.engine.impl.identity.Authentication;
+import org.foxbpm.engine.impl.util.StringUtil;
 import org.foxbpm.portal.model.ExpenseEntity;
 import org.foxbpm.portal.service.ExpenseService;
 import org.foxbpm.rest.common.api.DataResult;
@@ -36,6 +39,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import jodd.util.Base64;
 
 @Controller
 public class ExpenseController extends AbstractController{
@@ -50,6 +55,12 @@ public class ExpenseController extends AbstractController{
 		try{
 			Map<String,Object> formData = getFormData(request);
 			UserEntity userEntity = (UserEntity)request.getSession().getAttribute("user");
+			
+			if (userEntity == null) {
+				SimulateLogin(response, request); //模拟登录，用于远程接口调用（见‘JavaRESTClient.java’）
+				userEntity = (UserEntity)request.getSession().getAttribute("user");
+			}
+			
 			expenseEntity.setOwner(userEntity.getUserId());
 			expenseService.applyNewExpense(expenseEntity,formData);
 			response.getWriter().print(showMessage("启动成功！",true));
@@ -102,5 +113,42 @@ public class ExpenseController extends AbstractController{
 				+ "</script>";
 		
 		return result;
+	}
+	
+	/**
+	 * 模拟用户登录
+	 * @param response
+	 * @param request
+	 * @throws Exception
+	 */
+	private void SimulateLogin(HttpServletResponse response,
+			HttpServletRequest request) throws Exception {
+		// 从登录的口获取到用户名和密码
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("password");
+		// 该接口同时也是登出的口，当发现有特殊参数时则做登出操作。
+//		String logout = request.getParameter("doLogOut");
+		String contextPath = request.getContextPath();
+		
+		UserEntity userEntity = (UserEntity) Authentication.selectUserByUserId(userName);
+		if (null != userEntity && StringUtil.equals(password, userEntity.getPassword())) {
+			// 这里约定了一个参数，流程引擎在运行时会默认从session里按照这两个key来获取参数，如果替换了登录的方式，请保证这两个key依然可以获取到正确的数据
+			request.getSession().setAttribute("userId", userEntity.getUserId());
+			request.getSession().setAttribute("user", userEntity);
+			
+			 // 生成base 64位验证码
+	        String base64Code = "Basic " + Base64.encodeToString(userEntity.getUserId() + ":" + userEntity.getPassword());
+	        request.getSession().setAttribute("BASE_64_CODE", base64Code);
+			
+			Cookie userIdCookie = new Cookie("userId",  userEntity.getUserId());
+			userIdCookie.setMaxAge(-1);
+			response.addCookie(userIdCookie);
+			
+//			response.sendRedirect(contextPath + targetUrl);
+		} else {
+			response.setContentType("text/html;charset=utf-8");
+			response.getWriter().print("<script>alert('用户名或密码错误！');window.location.href='" + contextPath + "/login.html';</script>");
+		}
+
 	}
 }
